503907d62ded7a785a6c2c488ae4d2ac4e583633
Valeriy Lvov <tepliy_shov@mail.ru>
2024-07-07 22:43:55 +0500

    update

---
+++ homework2.py
@@ -0,0 +1,6 @@
+summ_of_homework = 12
+summ_of_hours = 1.5
+urban_course = 'Python'
+time_to_one_task = summ_of_hours / summ_of_homework
+print(f'Курс: {urban_course}, всего задач: {summ_of_homework}, затрачено часов: {summ_of_hours}, '
+      f'среднее время выполнения {time_to_one_task}')
