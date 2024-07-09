bb58ad70ba406ce00f5ff5acd551d6d98733ec60
Valeriy Lvov <tepliy_shov@mail.ru>
2024-07-09 18:32:04 +0500

    add homework3.py

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = "Валерий"
+print(f"My name is {name}")
+age = 34
+print(f"I'm {age} years old")
+agt = 35
+print(f"My new age is {age} years old")
+is_student = True
+print(f"I'm a student: {is_student}")
